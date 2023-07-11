987eb1ce6e68f99f09bc0410740694d38416588f
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-12 00:18:35 +0900

    feat: 🎸 isMarketing 유저의 칼럼에 추가

--- tutice/src/main/java/gwasuwonshot/tutice/user/dto/assembler/UserAssembler.java
+++ tutice/src/main/java/gwasuwonshot/tutice/user/dto/assembler/UserAssembler.java
@@ -1,5 +1,24 @@
 package gwasuwonshot.tutice.user.dto.assembler;
 
+import gwasuwonshot.tutice.user.entity.Provider;
+import gwasuwonshot.tutice.user.entity.Role;
+import gwasuwonshot.tutice.user.entity.User;
+import org.springframework.stereotype.Component;
+
 @Component
 public class UserAssembler {
+
+    public User toEntity(String email, Provider provider, String password,
+                         String name, String deviceToken, Role role, Boolean isMarketing){
+        return User.builder()
+                .email(email)
+                .provider(provider)
+                .password(password)
+                .name(name)
+                .deviceToken(deviceToken)
+                .role(role)
+                .isMarketing(isMarketing)
+                .build();
+
+    }
 }
