3a7b137300af819e794f3a77c7abb6a4947e445e
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-13 01:56:25 +0900

    feat: 🎸 creatRessonRequestDto 등등에 정규표현 valid 추가

--- src/main/java/gwasuwonshot/tutice/lesson/dto/request/CreateLessonRequestRegularSchedule.java
+++ src/main/java/gwasuwonshot/tutice/lesson/dto/request/CreateLessonRequestRegularSchedule.java
@@ -6,20 +6,33 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateLessonRequestRegularSchedule {
     @NotBlank
     @Schema(description = "요일")
+    @Pattern(
+            regexp="(월|화|수|목|금|토|일)", //요일 정보
+            message = "요일은 월,화,수,목,금,토,일 중 하나여야 합니다."
+    )
     private String dayOfWeek;
 
     @NotBlank
     @Schema(description = "시작 시간")
+    @Pattern(
+            regexp="(0[0-9]|1[0-9]|2[0-4]):(0[1-9]|[0-5][0-9])", //시간정보
+            message = "시간의 형태는 HH:mm의 형태여야합니다."
+    )
     private String startTime;
 
     @NotBlank
     @Schema(description = "종료 시간")
+    @Pattern(
+            regexp="(0[0-9]|1[0-9]|2[0-4]):(0[1-9]|[0-5][0-9])", //시간정보
+            message = "시간의 형태는 HH:mm의 형태여야합니다."
+    )
     private String endTime;
 }
 
--- src/main/java/gwasuwonshot/tutice/lesson/dto/response/CreateLessonResponseDto.java
+++ src/main/java/gwasuwonshot/tutice/lesson/dto/response/CreateLessonResponseDto.java
@@ -6,10 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 
 @Getter
