b108df45414ea35336f0d9a38f032b9405339be2
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-13 01:28:00 +0900

    feat: 🎸 기획과 상의후 유저 계좌의 은행정보를 텍스트로 변경

--- src/main/java/gwasuwonshot/tutice/user/entity/Bank.java
+++ src/main/java/gwasuwonshot/tutice/user/entity/Bank.java
@@ -6,16 +6,18 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Bank {
-    SINHAN("BANK_SINHAN","신한은행"),
-    KB("BANK_KB","KB국민은행"),
-    WOORI("BANK_WOORI","우리은행"),
-    HANA("BANK_HANA","하나은행"),
-    SC("BANK_SC","SC제일은행"),
-    K("BANK_K","케이뱅크"),
-    KAKAO("BANK_KAKAO","카카오뱅크"),
-    TOSS("BANK_TOSS","토스뱅크"),
-    NH("BANK_NH","NH농협은행");
 
-    private final String key;
-    private final String value;
+    //기획과의 논의를 통해 은행정보는 텍스트로만 저장하는것으로 변경
+//    SINHAN("BANK_SINHAN","신한은행"),
+//    KB("BANK_KB","KB국민은행"),
+//    WOORI("BANK_WOORI","우리은행"),
+//    HANA("BANK_HANA","하나은행"),
+//    SC("BANK_SC","SC제일은행"),
+//    K("BANK_K","케이뱅크"),
+//    KAKAO("BANK_KAKAO","카카오뱅크"),
+//    TOSS("BANK_TOSS","토스뱅크"),
+//    NH("BANK_NH","NH농협은행");
+//
+//    private final String key;
+//    private final String value;
 }
