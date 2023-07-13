f83eb680e68cafcf4f872c6f5c5dcb4fde404094
sojungpp <sophie2458@naver.com>
2023-07-13 19:57:29 +0900

    feat: 🎸 enum 유효성 검사 어노테이션 생성

---
+++ src/main/java/gwasuwonshot/tutice/common/resolver/enumValue/EnumValidator.java
@@ -0,0 +1,28 @@
+package gwasuwonshot.tutice.common.resolver.enumValue;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<Enum, String> {
+
+    private Enum annotation;
+
+    @Override
+    public void initialize(Enum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues != null) {
+            for (Object enumValue : enumValues) {
+                if (value.equals(enumValue.toString())
+                        || (this.annotation.ignoreCase() && value.equalsIgnoreCase(enumValue.toString()))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
