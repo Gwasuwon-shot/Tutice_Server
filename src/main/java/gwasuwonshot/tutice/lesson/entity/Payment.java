1a4eb4c573618c23172df236d65d048665610acb
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-14 03:36:23 +0900

    fix: 🐛 enumValidator에서 enum의 value를 가져오게끔 수정

--- src/main/java/gwasuwonshot/tutice/lesson/entity/Payment.java
+++ src/main/java/gwasuwonshot/tutice/lesson/entity/Payment.java
@@ -1,11 +1,12 @@
 package gwasuwonshot.tutice.lesson.entity;
 
+import gwasuwonshot.tutice.common.resolver.enumValue.EnumModel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum Payment {
+public enum Payment implements EnumModel {
 
     PRE_PAYMENT("PAYMENT_PRE","선불"),
     POST_PAYMENT("PAYMENT_POST","후불");
--- src/main/java/gwasuwonshot/tutice/user/entity/Provider.java
+++ src/main/java/gwasuwonshot/tutice/user/entity/Provider.java
@@ -1,11 +1,12 @@
 package gwasuwonshot.tutice.user.entity;
 
+import gwasuwonshot.tutice.common.resolver.enumValue.EnumModel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum Provider {
+public enum Provider implements EnumModel {
     LOCAL("PROVIDER_LOCAL","자체"),
     KAKAO("PROVIDER_KAKAO","카카오");
     private final String key;
