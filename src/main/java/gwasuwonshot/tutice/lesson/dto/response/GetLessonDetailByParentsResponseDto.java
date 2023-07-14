25a952ae0c98e2cc5be92934329ffb6ef2ff8a4b
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-15 04:04:27 +0900

    refactor: 💡 dto를 좀더 깔끔하게 하기위해 domain 객체를 사용

--- src/main/java/gwasuwonshot/tutice/lesson/dto/response/GetLessonDetailByParentsResponseDto.java
+++ src/main/java/gwasuwonshot/tutice/lesson/dto/response/GetLessonDetailByParentsResponseDto.java
@@ -1,5 +1,7 @@
 package gwasuwonshot.tutice.lesson.dto.response;
 
+import gwasuwonshot.tutice.common.module.DateAndTimeConvert;
+import gwasuwonshot.tutice.lesson.entity.Lesson;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,14 +12,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetLessonDetailByParentsResponseDto {
 
-    private GetLessonDetailByParentsResponseLesson lesson;
+    private Long idx;
+    private String teacherName;
     private String startDate;
     private String payment;
     private Long amount;
+    private GetLessonDetailByParentsResponseAccount account;
 
-    public static GetLessonDetailByParentsResponseDto of(GetLessonDetailByParentsResponseLesson lesson,
-                                                         String startDate, String payment, Long amount) {
-        return new GetLessonDetailByParentsResponseDto(lesson, startDate,payment,amount);
+    // TODO : 근데 이런 도메인 클래스를 dto 에서 그대로 쓰면 결합도가 높아지는게 아닌지?
+    public static GetLessonDetailByParentsResponseDto of(Lesson lesson) {
+        return new GetLessonDetailByParentsResponseDto(lesson.getIdx(),
+                lesson.getTeacher().getName(),
+                DateAndTimeConvert.dateConvertString(lesson.getStartDate()),
+                lesson.getPayment().getValue(),
+                lesson.getAmount(),
+                GetLessonDetailByParentsResponseAccount.of(lesson.getAccount()));
     }
 
 }
