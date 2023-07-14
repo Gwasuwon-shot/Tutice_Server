82f5b6d36493752a1eefd8b152c628b92a3262a7
sojungpp <sophie2458@naver.com>
2023-07-14 23:35:42 +0900

    feat: 🎸 localLogin 로직 구현

--- src/main/java/gwasuwonshot/tutice/user/controller/AuthController.java
+++ src/main/java/gwasuwonshot/tutice/user/controller/AuthController.java
@@ -2,6 +2,7 @@ package gwasuwonshot.tutice.user.controller;
 
 import gwasuwonshot.tutice.common.dto.ApiResponseDto;
 import gwasuwonshot.tutice.common.exception.SuccessStatus;
+import gwasuwonshot.tutice.user.dto.request.LocalLoginRequestDto;
 import gwasuwonshot.tutice.user.dto.request.LocalSignUpRequestDto;
 import gwasuwonshot.tutice.user.dto.response.LoginResponseDto;
 import gwasuwonshot.tutice.user.service.UserService;
@@ -22,6 +23,12 @@ public class AuthController {
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponseDto<LoginResponseDto> localSignUp(@RequestBody @Valid final LocalSignUpRequestDto request) {
         return ApiResponseDto.success(SuccessStatus.SIGNUP_SUCCESS, userService.localSignUp(request));
+    }
+
+    @PostMapping("/local/login")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponseDto<LoginResponseDto> localLogin(@RequestBody @Valid final LocalLoginRequestDto request) {
+        return ApiResponseDto.success(SuccessStatus.LOGIN_SUCCESS, userService.localLogin(request));
     }
 
 }
