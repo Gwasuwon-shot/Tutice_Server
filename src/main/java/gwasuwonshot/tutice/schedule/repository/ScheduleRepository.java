0bfa4748ceb42bb8248b52887678b575ff606eb2
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-17 03:14:26 +0900

    feat: 🎸 레슨의 가장 최근 스케쥴을 가져오는 로직 스케쥴레포에 추가

--- src/main/java/gwasuwonshot/tutice/schedule/repository/ScheduleRepository.java
+++ src/main/java/gwasuwonshot/tutice/schedule/repository/ScheduleRepository.java
@@ -2,6 +2,7 @@ package gwasuwonshot.tutice.schedule.repository;
 
 import gwasuwonshot.tutice.lesson.entity.Lesson;
 import gwasuwonshot.tutice.schedule.entity.Schedule;
+import gwasuwonshot.tutice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -10,4 +11,8 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByDateBetweenAndLessonInOrderByDate(LocalDate startDate, LocalDate endDate, List<Lesson> lessonList);
     List<Schedule> findAllByDateAndLessonIn(LocalDate now, List<Lesson> lessonList);
+
+    Schedule findTopByLessonOrderByDateDesc(Lesson lesson);
+
+
 }
