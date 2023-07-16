98f3da00a8741404e0e889d46d65dcc265b78bae
sojungpp <sophie2458@naver.com>
2023-07-17 03:39:31 +0900

    feat: 🎸 getTodayScheduleByTeacher dto 추가

---
+++ src/main/java/gwasuwonshot/tutice/schedule/dto/response/TodaySchedule.java
@@ -0,0 +1,22 @@
+package gwasuwonshot.tutice.schedule.dto.response;
+
+import gwasuwonshot.tutice.schedule.entity.Schedule;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class TodaySchedule {
+    private TodayScheduleByTeacher lesson;
+    private TodayScheduleDetailByTeacher schedule;
+    private Integer timeStatus;
+
+    public static TodaySchedule of(Schedule schedule, Integer timeStatus, Integer scheduleCount) {
+        return TodaySchedule.builder()
+                .lesson(TodayScheduleByTeacher.of(schedule.getLesson().getIdx(), schedule.getLesson().getStudentName(), schedule.getLesson().getSubject()))
+                .schedule(TodayScheduleDetailByTeacher.of(schedule.getIdx(), schedule.getStatus().getValue(), scheduleCount))
+                .timeStatus(timeStatus)
+                .build();
+    }
+}
