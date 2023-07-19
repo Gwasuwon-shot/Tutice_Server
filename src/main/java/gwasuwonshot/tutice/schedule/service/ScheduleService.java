0ef6eb5c776efcbd2481a207aafc25e4dc45f6f8
sojungpp <sophie2458@naver.com>
2023-07-19 22:41:35 +0900

    fix: 🐛 미래 스케줄 기준에 오늘 출결 기준 추가

--- src/main/java/gwasuwonshot/tutice/schedule/service/ScheduleService.java
+++ src/main/java/gwasuwonshot/tutice/schedule/service/ScheduleService.java
@@ -287,7 +287,7 @@ public class ScheduleService {
         Schedule schedule = scheduleRepository.findById(request.getSchedule().getIdx())
                 .orElseThrow(() -> new NotFoundUserException(ErrorStatus.NOT_FOUND_SCHEDULE_EXCEPTION, ErrorStatus.NOT_FOUND_SCHEDULE_EXCEPTION.getMessage()));
         // 미래 스케줄은 수정 불가
-        if(schedule.getDate().isAfter(LocalDate.now())) throw new InvalidAttendanceDateException(ErrorStatus.INVALID_ATTENDANCE_DATE_EXCEPTION, ErrorStatus.INVALID_ATTENDANCE_DATE_EXCEPTION.getMessage());
+        if(schedule.getDate().isAfter(LocalDate.now()) || schedule.getDate().isEqual(LocalDate.now()) && schedule.getStartTime().isAfter(LocalTime.now())) throw new InvalidAttendanceDateException(ErrorStatus.INVALID_ATTENDANCE_DATE_EXCEPTION, ErrorStatus.INVALID_ATTENDANCE_DATE_EXCEPTION.getMessage());
         // 취소 상태에서는 수정 불가
         if(schedule.getStatus().equals(ScheduleStatus.CANCEL)) throw new InvalidAttendanceStatusException(ErrorStatus.INVALID_ATTENDANCE_STATUS_EXCEPTION, ErrorStatus.INVALID_ATTENDANCE_STATUS_EXCEPTION.getMessage());
         // 이전 스케줄 누락되면 불가
