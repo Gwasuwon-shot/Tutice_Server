826a976a731630872960419675d315ea5773e43d
sojungpp <sophie2458@naver.com>
2023-07-20 04:03:44 +0900

    fix: 🐛 ATTENDANCE_IMMEDIATE_CHECK 제목 변경

--- src/main/java/gwasuwonshot/tutice/user/entity/NotificationConstant.java
+++ src/main/java/gwasuwonshot/tutice/user/entity/NotificationConstant.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationConstant{
-    ATTENDANCE_IMMEDIATE_CHECK("출결 체크할 시간이에요.","수업이 종료되었네요! 출결을 입력해주세요."),
+    ATTENDANCE_IMMEDIATE_CHECK("출결체크할 시간이에요.","수업이 종료되었네요! 출결을 입력해주세요."),
     ATTENDANCE_LATE_CHECK("아직 출결체크가 되지 않았어요!"," 출결체크, 더 잊기 전에 지금 체크해보세요."),
     REQUEST_ATTENDANCE("아"," 요."),
     REQUEST_PAYMENT_RECORD("아직 출결되지 않았어요!"," 출결체크, 크해보세요.");
