3966642644587cf5ac4c93283cfb3331252c0f64
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-07-19 17:28:37 +0900

    feat: 🎸 getLessonByTeacher 성공메시지와 responseDto 제작

--- src/main/java/gwasuwonshot/tutice/common/exception/SuccessStatus.java
+++ src/main/java/gwasuwonshot/tutice/common/exception/SuccessStatus.java
@@ -27,6 +27,7 @@ public enum SuccessStatus {
     GET_PAYMENT_RECORD_POST_VIEW_SUCCESS(HttpStatus.OK,"입금 등록 뷰 정보 가져오기 성공"),
     UPDATE_PAYMENT_RECORD_SUCCESS(HttpStatus.OK,"입금 등록 성공"),
     GET_PAYMENT_RECORD_SUCCESS(HttpStatus.OK,"입금 내역 가져오기 성공"),
+    GET_LESSON_BY_TEACHER_SUCCESS(HttpStatus.OK,"선생님 수업 관리 뷰 조회 성공"),
 
 
 
