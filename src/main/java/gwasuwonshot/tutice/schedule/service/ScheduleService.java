cb0a95e882a2f307c1f9b1e87ac60901c9ebb657
sojungpp <sophie2458@naver.com>
2023-08-27 22:55:09 +0900

    refactor:💡 getLatestScheduleByTeacher API response 분리

--- src/main/java/gwasuwonshot/tutice/schedule/service/ScheduleService.java
+++ src/main/java/gwasuwonshot/tutice/schedule/service/ScheduleService.java
@@ -339,20 +339,8 @@ public class ScheduleService {
         // 가장 최근 스케줄 가져오기 (오늘 포함)
         List<Lesson> lessonList = lessonRepository.findAllByTeacherIdxAndIsFinished(userIdx, false);
         List<Schedule> scheduleList = scheduleRepository.findAllByLessonInAndDateGreaterThanEqualOrderByDate(lessonList, LocalDate.now());
-        // 출석누락 유무
-        boolean isMissingAttendance = false;
-        boolean isAfterMissingAttendance = scheduleRepository.existsByStatusAndDateIsBeforeAndLessonIn(ScheduleStatus.NO_STATUS, LocalDate.now(), lessonList);
-        boolean isTodayMissingAttendance = scheduleRepository.existsByStatusAndDateAndStartTimeLessThanEqualAndLessonInOrderByDate(ScheduleStatus.NO_STATUS, LocalDate.now(), LocalTime.now(), lessonList);
-        if(isAfterMissingAttendance || isTodayMissingAttendance) isMissingAttendance = true;
-        // 수업연장 여부 유무
-        boolean isMissingMaintenance = false;
-        for(Lesson lesson : lessonList) {
-            if(isMissingMaintenance) break;
-            isMissingMaintenance = !scheduleRepository.existsByLessonAndCycleAndStatus(lesson, lesson.getCycle(), ScheduleStatus.NO_STATUS);
-        }
         // 오늘인지 체크
-        if(scheduleList.isEmpty()) return GetLatestScheduleByTeacherResponseDto.of(isMissingAttendance, isMissingMaintenance);
-        boolean isTodaySchedule = scheduleList.get(0).getDate().equals(LocalDate.now());
+        if(scheduleList.isEmpty()) return null;
         List<Schedule> latestScheduleList = new ArrayList<>();
         // 날짜 달라지면 그만
         LocalDate standardDate = scheduleList.get(0).getDate();
@@ -361,7 +349,7 @@ public class ScheduleService {
             else break;
         }
         latestScheduleList.sort(Comparator.comparing(Schedule::getStartTime));
-        return GetLatestScheduleByTeacherResponseDto.ofSchedule(isMissingAttendance, isMissingMaintenance, isTodaySchedule, standardDate, latestScheduleList);
+        return GetLatestScheduleByTeacherResponseDto.of(standardDate, latestScheduleList);
     }
 
     @Transactional
