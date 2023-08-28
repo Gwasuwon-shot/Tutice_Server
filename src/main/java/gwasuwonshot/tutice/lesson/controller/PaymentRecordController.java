aa9a263d46fd00b6cc6548f60c3d57308658f08a
sojungpp <sophie2458@naver.com>
2023-08-29 00:10:43 +0900

    refactor:💡 getLessonPaymentRecordByTeacher, getLessonPaymentRecordByParents 삭제

--- src/main/java/gwasuwonshot/tutice/lesson/controller/PaymentRecordController.java
+++ src/main/java/gwasuwonshot/tutice/lesson/controller/PaymentRecordController.java
@@ -5,14 +5,8 @@ import gwasuwonshot.tutice.common.dto.ApiResponseDto;
 import gwasuwonshot.tutice.common.exception.SuccessStatus;
 import gwasuwonshot.tutice.common.module.DateAndTimeConvert;
 import gwasuwonshot.tutice.common.resolver.userIdx.UserIdx;
-import gwasuwonshot.tutice.lesson.dto.request.UpdateLessonParentsRequestDto;
 import gwasuwonshot.tutice.lesson.dto.request.UpdatePaymentRecordRequestDto;
-import gwasuwonshot.tutice.lesson.dto.request.createLesson.CreateLessonRequestDto;
-import gwasuwonshot.tutice.lesson.dto.response.CreateLessonResponseDto;
 import gwasuwonshot.tutice.lesson.dto.response.getPaymentRecord.GetPaymentRecord;
-import gwasuwonshot.tutice.lesson.dto.response.getPaymentRecord.GetPaymentRecordByParentsResponseDto;
-import gwasuwonshot.tutice.lesson.dto.response.getPaymentRecord.GetPaymentRecordByTeacherResponseDto;
-import gwasuwonshot.tutice.lesson.dto.response.getPaymentRecord.GetPaymentRecordByUserResponseDto;
 import gwasuwonshot.tutice.lesson.dto.response.getPaymentRecordView.GetPaymentRecordViewResponseDto;
 import gwasuwonshot.tutice.lesson.service.PaymentRecordService;
 import gwasuwonshot.tutice.user.entity.Role;
@@ -23,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -55,34 +48,6 @@ public class PaymentRecordController {
         return ApiResponseDto.success(SuccessStatus.GET_PAYMENT_RECORD_POST_VIEW_SUCCESS,
                 GetPaymentRecordViewResponseDto.of(paymentRecordService.getPaymentRecordView(userIdx, paymentRecordIdx),
                         DateAndTimeConvert.nowLocalDateConvertString()));
-
-
-    }
-
-    @GetMapping("/teacher/{lessonIdx}")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<GetPaymentRecordByUserResponseDto> getLessonPaymentRecordByTeacher(
-            @UserIdx final Long userIdx,
-            @PathVariable final Long lessonIdx) {
-
-
-        return ApiResponseDto.success(SuccessStatus.GET_PAYMENT_RECORD_SUCCESS,
-                paymentRecordService.getLessonPaymentRecordByUser(Role.TEACHER,userIdx,lessonIdx));
-
-
-    }
-
-
-    @GetMapping("/parents/{lessonIdx}")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<GetPaymentRecordByUserResponseDto> getLessonPaymentRecordByParents(
-            @UserIdx final Long userIdx,
-            @PathVariable final Long lessonIdx) {
-
-
-        return ApiResponseDto.success(SuccessStatus.GET_PAYMENT_RECORD_SUCCESS,
-                paymentRecordService.getLessonPaymentRecordByUser(Role.PARENTS, userIdx,lessonIdx));
-
 
     }
 
--- src/main/java/gwasuwonshot/tutice/lesson/dto/response/getPaymentRecord/GetPaymentRecord.java
+++ src/main/java/gwasuwonshot/tutice/lesson/dto/response/getPaymentRecord/GetPaymentRecord.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
