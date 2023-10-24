c1e9903bb98954635426d9dedfe9164a847d7e49
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-10-25 01:26:54 +0900

    fix: 🐛 dto에 isFinished 추가 및 서비스 코드에서 isFinished 읽어옴

--- src/main/java/gwasuwonshot/tutice/lesson/dto/response/getLessonByTeacher/GetLessonByTeacher.java
+++ src/main/java/gwasuwonshot/tutice/lesson/dto/response/getLessonByTeacher/GetLessonByTeacher.java
@@ -17,10 +17,11 @@ public class GetLessonByTeacher {
     private String studentName;
     private String subject;
     private Long percent;
+    private Boolean isFinished;
     private GetLessonByTeacherLatestRegularSchedule latestRegularSchedule;
 
-    public static GetLessonByTeacher of(Long idx, String studentName,String subject,Long percent, GetLessonByTeacherLatestRegularSchedule latestRegularSchedule) {
-        return new GetLessonByTeacher(idx, studentName, subject, percent, latestRegularSchedule);
+    public static GetLessonByTeacher of(Long idx, String studentName,String subject,Long percent, Boolean isFinished, GetLessonByTeacherLatestRegularSchedule latestRegularSchedule) {
+        return new GetLessonByTeacher(idx, studentName, subject, percent, isFinished, latestRegularSchedule);
 
     }
 }
