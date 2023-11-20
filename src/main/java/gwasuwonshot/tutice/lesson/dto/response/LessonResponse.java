a35e3c41f4aac6214741b5fa0e8c3baf838f6820
sojungpp <sophie2458@naver.com>
2023-11-20 19:25:38 +0900

    refactor:💡 getLessonByParents -> lessonResponse 공통화

---
+++ src/main/java/gwasuwonshot/tutice/lesson/dto/response/LessonResponse.java
@@ -0,0 +1,33 @@
+package gwasuwonshot.tutice.lesson.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class LessonResponse {
+    private Long idx;
+    private String teacherName;
+    private String studentName;
+    private String subject;
+    private Long count;
+    private Long nowCount;
+    private Long percent;
+
+    public static LessonResponse of(Long idx, String teacherName, String studentName,
+                                    String subject, Long count, Long nowCount , Long percent) {
+        return LessonResponse.builder()
+                .idx(idx)
+                .teacherName(teacherName)
+                .studentName(studentName)
+                .subject(subject)
+                .count(count)
+                .nowCount(nowCount)
+                .percent(percent)
+                .build();
+        }
+
+}
