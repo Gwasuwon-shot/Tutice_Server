0184a02f7c9ddf613de75bef40b07faa60fb71c2
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2024-04-03 14:05:31 +0900

    refactor: 💡 Annotation validator 에러메시지 ErrorStatus로 모음

--- src/main/java/gwasuwonshot/tutice/common/resolver/validator/createLessonValid/CreateLessonValid.java
+++ src/main/java/gwasuwonshot/tutice/common/resolver/validator/createLessonValid/CreateLessonValid.java
@@ -1,20 +1,21 @@
 package gwasuwonshot.tutice.common.resolver.validator.createLessonValid;
 
 
+import gwasuwonshot.tutice.common.exception.ErrorStatus;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Documented
-@Target({ TYPE })
+@Target({ TYPE , ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
 @Retention(RUNTIME)
 @Constraint(validatedBy = {CreateLessonValidator.class})
+
 public @interface CreateLessonValid {
-    String message() default "수업 시작시간이 종료시간보다 늦습니다. ";
+    String message() default ErrorStatus.INVALID_REGULAR_SCHEDULE_TIME_MESSAGE;
 
     Class<?>[] groups() default { };
 
